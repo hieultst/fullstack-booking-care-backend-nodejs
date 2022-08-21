905031971505fa5a8d97b8d37eb03555abb4a008
Hieult <infor.letrunghieu@gmail.com>
2022-08-21 21:58:55 +0700

    use ORM to edit data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -36,10 +36,34 @@ let displayGetCRUD = async (req, res) => {
     });
 };
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInforById(userId);
+        // check user data not found
+
+        return res.render("editCRUD.ejs", {
+            user: userData,
+        });
+    } else {
+        return res.send("User not found! ");
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUsers,
+    });
+};
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 };
